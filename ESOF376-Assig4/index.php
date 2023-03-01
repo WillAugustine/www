20a8191f50a1f9a67cd27cff607bbd83b2cddf17
Will Augustine <william_augustine@hotmail.com>
2023-03-01 15:40:41 -0700

    completed session validation

--- ESOF376-Assig4/index.php
+++ ESOF376-Assig4/index.php
@@ -1,74 +1,79 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "LOLzies101";
+    $servername = "localhost";
+    $username = "root";
+    $password = "LOLzies101";
 
-$conn = new mysqli($servername, $username, $password);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$sql = "CREATE DATABASE IF NOT EXISTS test_user";
-if ($conn->query($sql) === TRUE) {
-    // echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
-$conn->close();
+    $conn = new mysqli($servername, $username, $password);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "CREATE DATABASE IF NOT EXISTS test_user";
+    if ($conn->query($sql) === TRUE) {
+        // echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+    $conn->close();
 
-//echo sha1('aslam');
-if (isset($_REQUEST['attempt']))
-{
+    //echo sha1('aslam');
+    if (isset($_REQUEST['attempt']))
+    {
 
-$user = $_POST['user'];
-$password = sha1($_POST['password']);
+        $user = $_POST['user'];
+        $password = sha1($_POST['password']);
 
- 
- // connect to the database
-$connect = mysqli_connect( 'localhost', 'root', 'LOLzies101', 'test_user' );
-if ( !$connect ) exit( 'connection failed: ' . mysqli_connect_error() );
 
-// create a query statement resource
-$stmt = mysqli_prepare( $connect,
- "select user from users where user=? and password=?" );
+        // connect to the database
+        $connect = mysqli_connect( 'localhost', 'root', 'LOLzies101', 'test_user' );
+        if ( !$connect ) exit( 'connection failed: ' . mysqli_connect_error() );
 
-if ( $stmt ) {
- // bind the substitution to the statement
- mysqli_stmt_bind_param( $stmt, "ss", $user, $password );
- // execute the statement
- mysqli_stmt_execute( $stmt );
+        // create a query statement resource
+        $stmt = mysqli_prepare( $connect,
+        "select user from users where user=? and password=?" );
 
- // retrieve the result...
- mysqli_stmt_bind_result( $stmt, $major );
- 
-// ...and display it
- if ( mysqli_stmt_fetch( $stmt ) ) {
- session_start();
-$_SESSION['user']= $user;
-header('location: insert.php');
- 
- } else {
-echo "User does not exists";
- } 
+        if ( $stmt ) {
+            // bind the substitution to the statement
+            mysqli_stmt_bind_param( $stmt, "ss", $user, $password );
+            // execute the statement
+            mysqli_stmt_execute( $stmt );
 
-// clean up statement resource
- mysqli_stmt_close( $stmt );
-}
-mysqli_close( $connect );
- 
-}
+            // retrieve the result...
+            mysqli_stmt_bind_result( $stmt, $major );
+
+            // ...and display it
+            if ( mysqli_stmt_fetch( $stmt ) ) {
+                // Regenerates session ID
+                if (!empty($_POST['password']) && sha1($_POST['password']) === $password) {
+                    session_regenerate_id();
+                    $_SESSION['auth'] = TRUE;
+                    session_start();
+                    $_SESSION['user']= $user;
+                    header('location: insert.php');
+                }
+                
+
+            } else {
+                echo "User does not exists";
+            } 
+
+            // clean up statement resource
+            mysqli_stmt_close( $stmt );
+        }
+        mysqli_close( $connect );
+
+    }
 
 ?>
 
 
 <form action="index.php?attempt" method="post">
-Username: <input type="text" name="user" /><br />
-Password: <input type="password" name="password" /><br />
-<input type="submit" value="Login" />
+    Username: <input type="text" name="user" /><br />
+    Password: <input type="password" name="password" /><br />
+    <input type="submit" value="Login" />
 </form>
 <body>
     <p>
         Need to create an account? <br>
         <a href=create_login.html>Create Account</a>
     </p>
-    
 </body>
--- ESOF376-Assig4/insert.php
+++ ESOF376-Assig4/insert.php
@@ -29,150 +29,174 @@
 * Desctiption: PHP script to validate form for Assignment2.html
 */
 
-session_start();
+    // Overrides global settings in php.ini
+    ini_set('session.use_only_cookies', TRUE);
+    ini_set('session.use_tans_sid', FALSE);
 
-if (!isset($_SESSION['user']))
-header('location: index.php');
-else
-{
-echo "you are logged in! ";
-echo "<a href=logout.php?session=logout>logout</a><br /> <br /> ";
-}
+    // Sets session timeout to 20 minutes
+    ini_set('session.cookie_lifetime', 1200);
 
-$servername = "localhost";
-$username = "root";
-$password = "LOLzies101";
-$dbname = "enrollment";
+    session_start();
 
-$conn = new mysqli($servername, $username, $password);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$sql = "CREATE DATABASE IF NOT EXISTS enrollment";
-if ($conn->query($sql) === TRUE) {
-    // echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
-$conn->close();
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-}
-$sql = "CREATE TABLE IF NOT EXISTS personalinfo (
-    id VARCHAR(4) NOT NULL UNIQUE,
-    name VARCHAR(25) NOT NULL,
-    email VARCHAR(255),
-    age INT(3),
-    createdBy VARCHAR(255),
-    PRIMARY KEY (id)
-)";
-if ($conn->query($sql) === TRUE) {
-    // echo "Database created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-$conn->close();
-
-$expected = array('sid'=>'id', 'name'=>'string', 'email'=>'email', 'age'=>'int');
-if (isset($_POST['sid'], $_POST['name'], $_POST['email'], $_POST['age'])) {
-    $valid_id = $_POST['sid'];
-    $valid_name = $_POST['name'];
-    $valid_email = $_POST['email'];
-    $valid_age = $_POST['age'];
-    $session_user = $_SESSION['user'];
-    foreach ( $expected AS $key => $type ) {
-        if (empty( $_POST[$key])) {
-            echo "Key is empty!" . "<br />";
-            ${$key} = NULL;
-            continue;
-        }
-        switch( $type ) {
-            case 'id':
-                if (strlen($_POST[$key]) != 4) {
-                    ${$key} = $_POST[$key];
-                    echo "Invalid ID - not four characters : " . ${$key} . "<br />";
-                    break;
-                }
-                if (!preg_match("/^\D\d\d\d/", $_POST[$key])) {
-                    ${$key} = $_POST[$key];
-                    echo "Invalid ID - format should be 'LNNN' where L = letter and N = number!<br />";
-                    break;
-                }
-                else if (strlen($_POST[$key]) == 4) {
-                    ${$key} = $_POST[$key];
-                    // echo "Valid ID : " . ${$key} . "<br />";
-                    $valid_id = ${$key};
-                }
-                break;
-
-            case 'string':
-                if(!preg_match("/^([a-zA-Z' ]+)$/", $_POST[$key])) {
-                    ${$key} = $_POST[$key];
-                    echo "Invalid name - not alphabetic!<br />";
-                    break;
-                }
-                if (strlen($_POST[$key]) > 25) {
-                    ${$key} = $_POST[$key];
-                    echo "Invalid name - exceeds 25 characters!<br />";
-                    break;
-                }
-                ${$key} = $_POST[$key];
-                // echo "Valid name : " . ${$key} . "<br />";
-                $valid_name = ${$key};
-                break;
-
-            case 'email':
-                if (filter_var($_POST[$key], FILTER_VALIDATE_EMAIL)) {
-                    ${$key} = $_POST[$key];
-                    // echo "Valid email : " . ${$key} . "<br />";
-                    $valid_email = ${$key};
-                }
-                else {
-                    echo "Invalid email - incorrect format!<br />";
-                }
-                break;
-
-            case 'int':
-                if ($_POST[$key] < 16) {
-                    ${$key} = $_POST[$key];
-                    echo "Invalid age - must be 16 or older!<br />";
-                }
-                else if ($_POST[$key] > 100) {
-                    ${$key} = $_POST[$key];
-                    echo "Invalid age - must be 100 or younger!<br />";
-                }
-                else {
-                    ${$key} = $_POST[$key];
-                    // echo "Valid age : " . ${$key} . "<br /><br />";
-                    $valid_age = ${$key};
-                }
-                break;
-
-        }
-        if (!isset(${$key})) {
-            ${$key} = NULL;
-        }
+    if (!isset($_SESSION['user'])) 
+    header('location: index.php');
+    else
+    {
+        echo "you are logged in! ";
+        echo "<a href=logout.php?session=logout>logout</a><br /> <br /> ";
     }
 
-// echo "valid_id: '$valid_id'<br />";
-// echo "valid_name: '$valid_name'<br />";
-// echo "valid_email: '$valid_email'<br />";
-// echo "valid_age: '$valid_age'<br />";
-
-
-    $sql = $conn->prepare("INSERT INTO personalinfo (id, name, email, age, createdBy) VALUES (?, ?, ?, ?, ?)");
-    $sql->bind_param('sssis', $valid_id, $valid_name, $valid_email, $valid_age, $session_user);
-    $sql->execute();
-    $result = $sql->error;
-    // echo "Result: '$result'";
-    if ($result == '') {
-        echo "New record created successfully<br />";
-    } 
-    else {
-        echo "Error: " . $result . "<br>";
+    // Regenerates session ID if session started more than 30 min ago
+    if (!isset($_SESSION['CREATED'])) {
+        $_SESSION['CREATED'] = time();
+    } else if (time() - $_SESSION['CREATED'] > 1800) {
+        session_regenerate_id(true);
+        $_SESSION['CREATED'] = time();
     }
-}
-$conn->close();
+
+    // Expires session if last request was more than 20 min ago
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1200)){
+        session_unset();
+        session_destroy();
+    }
+    $_SESSION['LAST_ACTIVITY'] = time();
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "LOLzies101";
+    $dbname = "enrollment";
+
+    // Creates enrollment database
+    $conn = new mysqli($servername, $username, $password);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "CREATE DATABASE IF NOT EXISTS enrollment";
+    if ($conn->query($sql) === TRUE) {
+        // echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+    $conn->close();
+
+    // Creates personalinfo table in enrollment database
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "CREATE TABLE IF NOT EXISTS personalinfo (
+        id VARCHAR(4) NOT NULL UNIQUE,
+        name VARCHAR(25) NOT NULL,
+        email VARCHAR(255),
+        age INT(3),
+        createdBy VARCHAR(255),
+        PRIMARY KEY (id)
+    )";
+    if ($conn->query($sql) === TRUE) {
+        // echo "Database created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+    // Handles form
+    $expected = array('sid'=>'id', 'name'=>'string', 'email'=>'email', 'age'=>'int');
+    if (isset($_POST['sid'], $_POST['name'], $_POST['email'], $_POST['age'])) {
+        $valid_id = $_POST['sid'];
+        $valid_name = $_POST['name'];
+        $valid_email = $_POST['email'];
+        $valid_age = $_POST['age'];
+        $session_user = $_SESSION['user'];
+        foreach ( $expected AS $key => $type ) {
+            if (empty( $_POST[$key])) {
+                echo "Key is empty!" . "<br />";
+                ${$key} = NULL;
+                continue;
+            }
+            switch( $type ) {
+                case 'id':
+                    if (strlen($_POST[$key]) != 4) {
+                        ${$key} = $_POST[$key];
+                        echo "Invalid ID - not four characters : " . ${$key} . "<br />";
+                        break;
+                    }
+                    if (!preg_match("/^\D\d\d\d/", $_POST[$key])) {
+                        ${$key} = $_POST[$key];
+                        echo "Invalid ID - format should be 'LNNN' where L = letter and N = number!<br />";
+                        break;
+                    }
+                    else if (strlen($_POST[$key]) == 4) {
+                        ${$key} = $_POST[$key];
+                        // echo "Valid ID : " . ${$key} . "<br />";
+                        $valid_id = ${$key};
+                    }
+                    break;
+
+                case 'string':
+                    if(!preg_match("/^([a-zA-Z' ]+)$/", $_POST[$key])) {
+                        ${$key} = $_POST[$key];
+                        echo "Invalid name - not alphabetic!<br />";
+                        break;
+                    }
+                    if (strlen($_POST[$key]) > 25) {
+                        ${$key} = $_POST[$key];
+                        echo "Invalid name - exceeds 25 characters!<br />";
+                        break;
+                    }
+                    ${$key} = $_POST[$key];
+                    // echo "Valid name : " . ${$key} . "<br />";
+                    $valid_name = ${$key};
+                    break;
+
+                case 'email':
+                    if (filter_var($_POST[$key], FILTER_VALIDATE_EMAIL)) {
+                        ${$key} = $_POST[$key];
+                        // echo "Valid email : " . ${$key} . "<br />";
+                        $valid_email = ${$key};
+                    }
+                    else {
+                        echo "Invalid email - incorrect format!<br />";
+                    }
+                    break;
+
+                case 'int':
+                    if ($_POST[$key] < 16) {
+                        ${$key} = $_POST[$key];
+                        echo "Invalid age - must be 16 or older!<br />";
+                    }
+                    else if ($_POST[$key] > 100) {
+                        ${$key} = $_POST[$key];
+                        echo "Invalid age - must be 100 or younger!<br />";
+                    }
+                    else {
+                        ${$key} = $_POST[$key];
+                        // echo "Valid age : " . ${$key} . "<br /><br />";
+                        $valid_age = ${$key};
+                    }
+                    break;
+
+            }
+            if (!isset(${$key})) {
+                ${$key} = NULL;
+            }
+        }
+
+        // echo "valid_id: '$valid_id'<br />";
+        // echo "valid_name: '$valid_name'<br />";
+        // echo "valid_email: '$valid_email'<br />";
+        // echo "valid_age: '$valid_age'<br />";
+        // echo "session_user: '$session_user'<br />";
+
+
+        $sql = $conn->prepare("INSERT INTO personalinfo (id, name, email, age, createdBy) VALUES (?, ?, ?, ?, ?)");
+        $sql->bind_param('sssis', $valid_id, $valid_name, $valid_email, $valid_age, $session_user);
+        $sql->execute();
+        $result = $sql->error;
+        if ($result == '') {
+            echo "New record created successfully<br />";
+        } 
+        else {
+            echo "Error: " . $result . "<br>";
+        }
+    }
+    $conn->close();
 ?>
